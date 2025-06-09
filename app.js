1bc885425f40891251f1d07ab0df48b549948d80
Romil Dubey <romildubey03@gmail.com>
2025-06-09 22:31:14 +0530

    Do Database Connection

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+main()
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/EasyStay');
+}
 let port = 3000;
 
 app.listen(port, () => {
