90f3e999bca3ae69e050599ec2abb6d550d4e59e
Romil Dubey <romildubey03@gmail.com>
2025-06-11 02:18:24 +0530

    route path updated

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require('method-override');
 app.set("view-engine", "ejs");
-app.set('views', path.join(__dirname, 'views/listing'));
+app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+const ejsMate = require('ejs-mate');
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, '/public')));
 
 main()
     .then(() => {
@@ -29,7 +32,7 @@ app.listen(port, () => {
 app.get('/listings', async (req, res) => {
   try {
     const listings = await Listing.find({});
-    res.render('index.ejs', { listings });
+    res.render('listing/index.ejs', { listings });
   } catch (error) {
     console.error('Error fetching listings:', error);
     res.status(500).send('Internal Server Error');
@@ -38,7 +41,7 @@ app.get('/listings', async (req, res) => {
 
 //create route to render the form for creating a new listing
 app.get('/listings/new', (req, res) => {
-  res.render('new.ejs');
+  res.render('listing/new.ejs');
 });
 
 //show route to render a single listing
@@ -48,7 +51,7 @@ app.get('/listings/:id', async (req, res) => {
     if (!listing) {
       return res.status(404).send('Listing not found');
     }
-    res.render('show.ejs', { listing });
+    res.render('listing/show.ejs', { listing });
   } catch (error) {
     console.error('Error fetching listing:', error);
     res.status(500).send('Internal Server Error');
@@ -83,7 +86,7 @@ app.get('/listings/:id/edit', async (req, res) => {
     if (!listing) {
       return res.status(404).send('Listing not found');
     }
-    res.render('edit.ejs', { listing });
+    res.render('listing/edit.ejs', { listing });
   } catch (error) {
     console.error('Error fetching listing for edit:', error);
     res.status(500).send('Internal Server Error');
