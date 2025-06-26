27c06ecac26e0140adcd3a99449d74d303766cef
Romil Dubey <romildubey03@gmail.com>
2025-06-26 13:09:24 +0530

    vercel path updated

--- api/listings.js
+++ api/listings.js
@@ -1,2 +1,7 @@
-const app = require('../app');
-module.exports = (req, res) => app(req, res);
+// api/listings.js
+const path = require("path");
+// make sure all `require(...)` calls inside app.js find the right files:
+process.chdir(path.resolve(__dirname, ".."));
+
+const app = require("../app");   // pull in your app.js
+module.exports = app;            // export it as the Serverless Function
--- app.js
+++ app.js
@@ -61,8 +61,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => res.redirect('/listings'));
-
 // Importing routes
 const listingRouter = require("./routes/listings.js");
 const reviewsRouter = require("./routes/reviews.js");
