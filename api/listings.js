73bbeca22c247bcff7a75562108284618473afb9
Romil Dubey <romildubey03@gmail.com>
2025-06-26 13:50:03 +0530

    vercel path updated

--- api/listings.js
+++ api/listings.js
@@ -1,6 +1,2 @@
-// api/listings.js
-const path = require("path");
-process.chdir(path.resolve(__dirname, ".."));
-
 const app = require("../app");   // your Express instance from app.js
 module.exports = app;
